6d952b818c9d4863ae2ba9455a5c16f9234a64a3
ikigai-bit <57860099+ikigai-bit@users.noreply.github.com>
2021-05-22 11:32:10 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,5 +1,11 @@
 <?php
-$conn=$mysqli_connect("sql6.freemysqlhosting.net","sql6413889","KzHb8YQ2fA","User_Management") or die("Peace");
-
+$conn=$mysqli_connect("sql6.freemysqlhosting.net","sql6413889","KzHb8YQ2fA","User_Management");
+$fname=$_REQUEST['First Name'];
+$lname=$_REQUEST['Last Name'];
+$email=$_REQUEST['E-Mail'];
+$sql="INSERT INTO examniee VALUES('$fname','$lname','$email')";
+if(mysqli_query($conn,$sql)){
+    echo"Store";
+}
 
 ?>
